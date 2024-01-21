a8f1814253a64d081f251c0a3f48fb974519d4d2
Sneha Supriya <codingsneha@gmail.com>
2024-01-21 15:49:55 +0530

    update

--- qachat.py
+++ qachat.py
@@ -10,13 +10,13 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 ## function to load Gemini Pro model and get repsonses
 model=genai.GenerativeModel("gemini-pro") 
 chat = model.start_chat(history=[])
+
 def get_gemini_response(question):
     response=chat.send_message(question,stream=True)
     return response
 
 ##initialize our streamlit app
 st.set_page_config(page_title="Q&A Demo")
-
 st.header("Gemini LLM Application")
 
 # Initialize session state for chat history if it doesn't exist
