1524e68d9de223a1fd4129958366425df86b78ac
Sneha Supriya <codingsneha@gmail.com>
2024-01-21 16:03:57 +0530

    update

--- app.py
+++ app.py
@@ -17,14 +17,14 @@ def get_gemini_response(question):
 
 ##initialize our streamlit app
 st.set_page_config(page_title="Q&A Demo")
-st.header("Gemini LLM Application")
+st.header("Conversational ChatBot")
 
 # Initialize session state for chat history if it doesn't exist
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
 
 input=st.text_input("Input: ",key="input")
-submit=st.button("Ask the question")
+submit=st.button("Send")
 
 if submit and input:
     response=get_gemini_response(input)
@@ -34,7 +34,7 @@ if submit and input:
     for chunk in response:
         st.write(chunk.text)
         st.session_state['chat_history'].append(("Bot", chunk.text))
-st.subheader("The Chat History is")
+st.subheader("Chat History:")
     
 for role, text in st.session_state['chat_history']:
     st.write(f"{role}: {text}")
