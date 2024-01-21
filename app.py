f73d3f1cbdd6fb4aff65e6bac5430660d33cecdb
Sneha Supriya <codingsneha@gmail.com>
2024-01-21 14:25:08 +0530

    update

--- app.py
+++ app.py
@@ -9,12 +9,6 @@ import textwrap
 
 import google.generativeai as genai
 
-from IPython.display import Markdown
-
-
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
