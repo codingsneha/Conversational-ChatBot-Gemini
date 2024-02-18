be4f94a32d65c181dbfaaac4232d6bafbe65cf34
Sneha Supriya <codingsneha@gmail.com>
2024-02-18 19:02:35 +0530

    upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from PIL import Image
 load_dotenv() ## loading all the environment variables
 
 import streamlit as st
@@ -11,29 +12,39 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model=genai.GenerativeModel("gemini-pro") 
 chat = model.start_chat(history=[])
 
+image = Image.open('yuu.png')
+
 def get_gemini_response(question):
     response=chat.send_message(question,stream=True)
     return response
 
 ##initialize our streamlit app
-st.set_page_config(page_title="Q&A App")
-st.header("Conversational ChatBot")
+st.set_page_config(page_title="Yuu")
+st.image(image, width=200)
+st.header("Chat with Yuu")
 
 # Initialize session state for chat history if it doesn't exist
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
 
+
 input=st.text_input("Input: ",key="input")
+output=""
+history=""
+
 submit=st.button("Send")
 
 if submit and input:
-    response=get_gemini_response(input)
+    for role, text in st.session_state['chat_history']:
+        history+=f"{role}: {text}"
+    response=get_gemini_response("Context: You have to act as a cute therapist chatting friend who's name is 'Yuu' and respond to the user maybe taking into account the chat history, User: "+input+" Here's the past chat history just for context: "+history)
     # Add user query and response to session state chat history
     st.session_state['chat_history'].append(("You", input))
     st.subheader("The Response is")
     for chunk in response:
         st.write(chunk.text)
-        st.session_state['chat_history'].append(("Bot", chunk.text))
+        output+=chunk.text
+    st.session_state['chat_history'].append(("Yuu", output))
 st.subheader("Chat History:")
     
 for role, text in st.session_state['chat_history']:
